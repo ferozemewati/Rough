777db04919eaff8d6ec8a66596e41f7178aa7299
Feroze Khan <87601912+ferozemewati@users.noreply.github.com>
2022-03-27 23:46:53 +0530

    Just checking somethings

---
+++ DearJava.java
@@ -0,0 +1,93 @@
+/*
+public class DearJava {
+    public static void main(String[] args) {
+        int fact=1;
+        int i=1;
+        while (i<=5){
+            fact = fact*i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
+*/
+
+/*
+public class DearJava {
+    public static void main(String[] args) {
+        int a=0,b=1,c;
+        System.out.println(a+" "+b+" ");
+        for (int i=2;i<=5; i++){
+            c=a+b;
+            System.out.println(c);
+            a=b;
+            b=c;
+        }
+    }
+}
+*/
+
+/*
+public class DearJava {
+    public static void main(String[] args){
+        int a=0, b=1, c;
+        System.out.print(a+" "+b);
+        for(int i=2; i<=5; i++){
+            c=a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+}
+*/
+
+
+/*
+public class DearJava {
+    public static void main(String[] args) {
+        int fact=1,i=1;
+        while (i<=5){
+            fact=fact*i;
+            i++;
+        }
+        System.out.println(fact);
+    }
+}
+*/
+
+/*
+public class DearJava {
+    public static void main(String[]args){
+        int a=0, b=1, c;
+        System.out.print(a+" "+b);
+        for (int i=2; i<=10; i++){
+            c=a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+}
+*/
+
+/*class A{
+    public void find_series(int n){
+        int a=0, b=1, c;
+        System.out.print(a+" "+b);
+        for (int i=2; i<=n; i++){
+            c=a+b;
+            System.out.print(" "+c);
+            a=b;
+            b=c;
+        }
+    }
+}
+public class DearJava {
+    public static void main(String[] args) {
+     A obj=new A();
+     obj.find_series(10);
+    }
+}*/
+
+
